e08356e2dc7b3fe3bc1600e1cce98990c7ec671b
Usernameposhalko12 <usernameposhalko6@gmail.com>
2025-09-13 23:56:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -870,7 +870,7 @@ function startDino() {
         imgStatus.textContent = "Гра запущена";
 
         window.addEventListener("keydown", keyHandler);
-        spawnIntervalId = setInterval(spawnCactus,625);
+        spawnIntervalId = setInterval(spawnCactus,650);
         spawnCactus();
         rafId = requestAnimationFrame(loop);
     }
